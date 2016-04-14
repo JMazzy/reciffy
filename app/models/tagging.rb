b2070c16c585cb88b144dd776593313a36223e05
Joshua Masland-Sarani <j.masland@gmail.com>
2016-04-14 17:43:04 -0400

    minor edit

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,11 +1,8 @@
 class Tagging < ActiveRecord::Base
-
   belongs_to :tag
   belongs_to :taggable, polymorphic: true
 
   validates :tag_id, presence: true
   validates :taggable_id, presence: true
   validates :taggable_type, presence: true
-
-
 end
