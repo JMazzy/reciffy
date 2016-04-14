79c6fe6a8448322aa94ebe6417e6c7d28c82b49f
Deepa Kackar <dkackar99@yahoo.com>
2016-04-14 15:46:19 -0700

    Resolved some issues of merging in recipe controller

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,2 +1,13 @@
 class Subscription < ActiveRecord::Base
+  belongs_to :subscription_requestor, 
+             :foreign_key => :subscriber_id,
+             :class_name => "User"
+
+  belongs_to :subscription_receiver,  
+             :foreign_key => :subscribed_id,
+             :class_name => "User"
+
+  validates :subscriber_id, 
+            :uniqueness => { :scope => :subscribed_id }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,25 @@ class User < ActiveRecord::Base
   has_many :taggings, as: :taggable
   has_many :tags, through: :taggings
 
+
+  # When acting as the initiator of the subsription
+  has_many :initiated_subscribe_requests, 
+           :foreign_key => :subsriber_id,
+           :class_name => "Subscription"
+
+  has_many :subscriptions, 
+           :through => :initiated_subscribe_requests,
+           :source => :subscription_receiver
+
+  # When acting as the recipient of the friending
+  has_many :received_subscription_requests,  
+           :foreign_key => :subscribed_id,
+           :class_name => "Subscription"
+
+  has_many :users_subscribed_by,         
+           :through => :received_subscribe_requests,
+           :source => :subscriberequestor  
+
+
   after_create :create_profile
 end
