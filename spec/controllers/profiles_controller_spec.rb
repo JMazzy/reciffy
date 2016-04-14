f2e52b47d62420496021aa66a1a13690950d5b08
Kelsey James <kelseybjames@gmail.com>
2016-04-14 16:07:43 -0700

    more tests

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -1,1 +1,36 @@
 require 'rails_helper'
+
+describe ProfilesController do
+  let(:user){ create(:user) }
+  let(:profile){ create(:profile, user: user) }
+
+  describe 'PUT #update' do
+    let(:put_update_profile) do
+      put :update,
+            user_id: user.id,
+            profile: attributes_for(
+            :profile,
+            bio: 'I am a strange loop',
+            tagline: 'I am a cool cat',
+            first_name: 'Foo',
+            last_name: 'Bar',
+            city: 'Reno',
+            state: 'NV')
+    end
+
+    context 'user signed in' do
+      before do
+        user
+        profile
+        create_session(user)
+      end
+
+      it 'profile updated if user signed in' do
+        put_update_profile
+        profile.reload
+        expect(user.profile.bio).to eq('I am a strange loop')
+      end
+    end
+  end
+
+end
