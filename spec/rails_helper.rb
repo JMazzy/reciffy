194a1261b2eb31e12dcd1236238f6cc2416beb87
Joshua Masland-Sarani <j.masland@gmail.com>
2016-04-14 19:43:23 -0400

    fixed weird factory girl bug

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
+
 require File.expand_path('../../config/environment', __FILE__)
+
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Prevent database truncation if the environment is production
@@ -48,7 +50,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = false
+  config.use_transactional_fixtures = true
 
   config.include FactoryGirl::Syntax::Methods
 
