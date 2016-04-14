10c321d707bb038480dfd21301d3637623b30e11
Joshua Masland-Sarani <j.masland@gmail.com>
2016-04-14 14:42:51 -0400

    tag associations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ActiveRecord::Base
   has_many :photos, dependent: :destroy
   has_many :taggings, as: :taggable
+  has_many :tags, through: :taggings
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,3 @@
 class Tag < ActiveRecord::Base
-  has_many :taggings, as: :taggable
-  has_many :tags, through: :taggings
+  has_many :taggings
 end
