a75979e82ff43c7622c87c5fdbe0592ba627ffed
Kelsey James <kelseybjames@gmail.com>
2016-04-14 12:02:18 -0700

    added more profile tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:email){ |n| "foo#{n}@bar.com" }
+    password 'testtest'
   end
 end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,15 +1,29 @@
 require 'rails_helper'
 
 describe Profile do
-  let(:user){ build(:user, id: 1) }
   let(:profile){ build(:profile) }
+  describe 'attributes' do
 
-  it 'is valid with default attributes' do
-    expect(profile).to be_valid
+    it 'is valid with default attributes' do
+      expect(profile).to be_valid
+    end
+
+    it 'is valid with null attributes' do
+      new_profile = build(:profile, bio: nil, tagline: nil, first_name: nil, last_name: nil, city: nil, state: nil)
+      expect(new_profile).to be_valid
+    end
+
+    it "can't create two profiles for one user" do
+      profile = build(:profile)
+      profile.save!
+      second_profile = build(:profile)
+      expect{ second_profile.save! }.to raise_error
+    end
   end
 
-  it 'is valid with null attributes' do
-    new_profile = build(:profile, bio: null, tagline: null, first_name: null, last_name: null, city: null, state: null)
-    expect(new_profile).to be_valid
+  describe 'associations' do
+    it 'responds to user' do
+      expect(profile).to respond_to(:user)
+    end
   end
 end
