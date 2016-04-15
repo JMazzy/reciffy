0580b5bea10ecd2de2a24fcdfcab9b6f09920ecd
Joshua Masland-Sarani <j.masland@gmail.com>
2016-04-14 21:12:52 -0400

    add tags to form

---
+++ app/controllers/taggings_controller.rb
@@ -0,0 +1,39 @@
+class TaggingsController < ApplicationController
+
+  def create
+    @tagging = Tagging.new( tagging_params )
+    respond_to do |format|
+      if @tagging.save
+        format.html { render nothing: true }
+        format.json { render json: @tagging.to_json }
+        flash[:success] = "#{params[:taggable_type.capitalize]} tagged!"
+      else
+        format.html { render nothing: true }
+        format.json { render json: @tagging.errors, status: :unprocessable_entity }
+        flash[:danger] = "Sorry, your tagging didn't get through"
+      end
+    end
+  end
+
+  def destroy
+    @tagging = Tagging.find(params[:id])
+    respond_to do |format|
+      if @tagging.destroy
+        format.html { render nothing: true }
+        format.json { render json: @tagging.to_json }
+        flash[:success] = "#{params[:taggable_type].capitalize} untagged!"
+      else
+        format.html { render nothing: true }
+        format.json { render json: @tagging.errors, status: :unprocessable_entity }
+        flash[:danger] = "Tagging could not be destroyed."
+      end
+    end
+  end
+
+  private
+
+  def tagging_params
+    params.require(:tagging).permit(:tag_id, :taggable_id, :taggable_type)
+  end
+
+end
