3b8410ccfeee0569b7e04d2de7061cd7ca59f3e8
Joshua Masland-Sarani <j.masland@gmail.com>
2016-04-15 15:41:58 -0400

    fixed signin/up flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def rails_to_bootstrap_flash(flash_type)
+    if flash_type == "alert"
+      "danger"
+    elsif flash_type == "notice"
+      "success"
+    else
+      flash_type
+    end
+  end
+
 end
