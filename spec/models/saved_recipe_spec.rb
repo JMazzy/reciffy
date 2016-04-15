5cc0241b0da96c99e000be09ef29f2e2f7f233b8
Kelsey James <kelseybjames@gmail.com>
2016-04-15 09:37:37 -0700

    starting saved recipe model tests

--- spec/models/saved_recipe_spec.rb
+++ spec/models/saved_recipe_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
+describe SavedRecipe do
+  before do
+    DatabaseCleaner.clean
+  end
 
-RSpec.describe SavedRecipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:saved_recipe){ build(:saved_recipe) }
+
+  describe 'attributes' do
+    it 'is valid with default attributes' do
+      expect(saved_recipe).to be_valid
+    end
+  end
+
+  describe 'associations' do
+    it 'responds to user' do
+      expect(saved_recipe).to respond_to(:user)
+    end
+
+    it 'responds to recipe' do
+      expect(saved_recipe).to respond_to(:recipe)
+    end
+  end
 end
