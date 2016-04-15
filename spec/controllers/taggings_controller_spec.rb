35735a27138ab6a4fb7563eea8fb91cb9cdfea37
Joshua Masland-Sarani <j.masland@gmail.com>
2016-04-15 12:11:50 -0400

    merged

--- spec/controllers/taggings_controller_spec.rb
+++ spec/controllers/taggings_controller_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe TaggingsController, type: :controller do
+describe TaggingsController, type: :controller do
+  before do
+    DatabaseCleaner.clean
+  end
 
+  let(:tagging) { create :tagging }
+
+  context "user logged in" do
+
+    login_user
+
+    describe "#create" do
+      it "creates a tagging" do
+        expect{ post :create, tagging: attributes_for(:tagging) }.to change{ Tag.count }.by(1)
+      end
+    end
+  end
+
+  context "user not logged in" do
+
+    describe "#create" do
+      it "does NOT create a tagging" do
+        binding.pry
+        expect{ post :create, tagging: attributes_for(:tagging) }.to change{ Tag.count }.by(0)
+      end
+    end
+  end
 end
--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -5,42 +5,82 @@ describe TagsController, type: :controller do
     DatabaseCleaner.clean
   end
 
-  login_user
-
   let(:tag) { create :tag }
 
-  describe "#index" do
-    it "gets all the tags" do
-      other_tag = create :tag
+  context "user logged in" do
 
-      get :index
+    login_user
 
-      expect(assigns(:tags)).to match_array [tag, other_tag]
+    describe "#index" do
+      it "gets all the tags" do
+        other_tag = create :tag
+
+        get :index
+
+        expect(assigns(:tags)).to match_array [tag, other_tag]
+      end
+
+      it "renders the index template" do
+        get :index
+
+        expect(response).to render_template :index
+      end
+
     end
 
-    it "renders the index template" do
-      get :index
+    describe "#show" do
+      it "gets one tag" do
+        get :show, id: tag.id
+        expect(assigns(:tag)).to match tag
+      end
 
-      expect(response).to render_template :index
+      it "renders the show template" do
+        get :show, id: tag.id
+        expect(response).to render_template :show
+      end
     end
 
+    describe "#create" do
+      it "creates a tag" do
+        expect{ post :create, tag: { name: "RecipeTag" } }.to change{ Tag.count }.by(1)
+      end
+    end
   end
 
-  describe "#show" do
-    it "gets one tag" do
-      get :show, id: tag.id
-      expect(assigns(:tag)).to match tag
+  context "user not logged in" do
+
+    describe "#index" do
+      it "does NOT get all the tags" do
+        other_tag = create :tag
+
+        get :index
+
+        expect(assigns(:tags)).not_to match_array [tag, other_tag]
+      end
+
+      it "does NOT render the index template" do
+        get :index
+
+        expect(response).not_to render_template :index
+      end
     end
 
-    it "renders the show template" do
-      get :show, id: tag.id
-      expect(response).to render_template :show
-    end
-  end
+    describe "#show" do
+      it "does NOT get tag" do
+        get :show, id: tag.id
+        expect(assigns(:tag)).not_to match tag
+      end
 
-  describe "#create" do
-    it "creates a tag" do
-      expect{ post :create, tag: { name: "RecipeTag" } }.to change{ Tag.count }.by(1)
+      it "does NOT show the show template" do
+        get :show, id: tag.id
+        expect(response).not_to render_template :show
+      end
+    end
+
+    describe "#create" do
+      it "does NOT create a tag" do
+        expect{ post :create, tag: { name: "RecipeTag" } }.to change{ Tag.count }.by(0)
+      end
     end
   end
 end
