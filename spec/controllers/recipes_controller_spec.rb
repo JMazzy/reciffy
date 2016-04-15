99f09f7323dcf37fefa109324fc18aac21e4d706
Kelsey James <kelseybjames@gmail.com>
2016-04-15 15:16:27 -0700

    some more tests

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -1,5 +1,70 @@
 require 'rails_helper'
 
 describe RecipesController, type: :controller do
+  let(:recipe){ create :recipe }
 
+  context 'user logged in' do
+    login_user
+
+    describe '#index' do
+      it 'gets all recipes' do
+        other_recipe = create :recipe
+        get :index
+        expect(assigns(:recipes)).to match_array [recipe, other_recipe]
+      end
+
+      it 'renders index template' do
+        get :index
+        expect(response).to render_template :index
+      end
+    end
+
+    describe '#show' do
+      it 'gets one recipe' do
+        get :show, id: recipe.id
+        expect(assigns(:recipe)).to match recipe
+      end
+
+      it 'renders show template' do
+        get :show, id: recipe.id
+        expect(response).to render_template :show
+      end
+    end
+
+    describe '#new' do
+      it 'renders new template' do
+        get :new
+        expect(response).to render_template(:new)
+      end
+    end
+
+    describe '#create' do
+      it 'creates a recipe' do
+        expect{ post :create, recipe: { name: 'Recipe', description: 'A new recipe', 
+                   prep_time: 5,
+                   cook_time: 5,
+                   original_id: 1,
+                   instructions: 'This is how you cook it' } }.to change{ Recipe.count }.by(1)
+      end
+    end
+  end
+
+  context 'user not logged in' do
+    describe '#new' do
+      it 'does not render new template' do
+        get :new
+        expect(response).not_to render_template(:new)
+      end
+    end
+
+    describe '#create' do
+      it 'does not create a recipe' do
+        expect{ post :create, recipe: { name: 'Recipe', description: 'A new recipe', 
+                   prep_time: 5,
+                   cook_time: 5,
+                   original_id: 1,
+                   instructions: 'This is how you cook it' } }.to change{ Recipe.count }.by(0)
+      end
+    end
+  end
 end
