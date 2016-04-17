d72ace387353d52fd742518c4a670e79e0e17228
Joshua Masland-Sarani <j.masland@gmail.com>
2016-04-16 21:10:31 -0400

    add recipe seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,13 +12,12 @@ test_user = User.create(  email: "foo@bar.com",
 test_user.profile.tags.create( name: "footag" )
 test_user.profile.tags.create( name: "bartag" )
 
-puts "Creating User"
+puts "Creating Users"
 
 test_user = User.create(  email: "foo@bar.com",
                           password: "foo1bar2" )
 
 10.times do |num|
-
   User.create(email: "user#{num}@gmail.com",
 		      password: "foo1bar2")
 end
@@ -36,4 +35,25 @@ puts "Creating Units"
 units = ["cup" ,"oz", "lbs", "grams", "tbsp", "tsp", "count"]
 units.each do |element|
  Unit.create(unit_type: element)
+end
+
+puts "Creating Recipes"
+
+10.times do
+  Recipe.create(  name: Faker::Hipster.words,
+                  description: Faker::Hipster.sentence,
+                  instructions: Faker::Hipster.sentences,
+                  prep_time: Random.rand(60),
+                  cook_time: Random.rand(60),
+                  user_id: User.all.sample.id )
+end
+
+Recipe.all.each do |r|
+  5.times do
+    r.recipe_ingredients.create(
+      ingredient_id: Ingredient.all.sample.id,
+      unit_id: Unit.all.sample.id,
+      quantity: Random.rand(4)
+    )
+  end
 end
