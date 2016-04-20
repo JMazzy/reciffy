4dec634e7d3c20f7d6b2f943e202d3bb7eae6e8b
Kelsey James <kelseybjames@gmail.com>
2016-04-20 07:59:59 -0700

    added tags to user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     respond_to do |format|
-      format.json { render json: @users.to_json( include: :profile )}
+      format.json { render json: @users.to_json( include: { profile: { include: :tags } } )}
     end
   end
 
