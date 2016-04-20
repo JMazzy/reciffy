0495c4332a3ea750fc4acaa7a0e4dab7ae92c538
Joshua Masland-Sarani <j.masland@gmail.com>
2016-04-20 14:57:57 -0400

    more work on creating recipes

--- app/assets/javascripts/controllers/recipe_index_controller.js
+++ app/assets/javascripts/controllers/recipe_index_controller.js
@@ -1,9 +1,17 @@
-reciffy.controller( 'RecipeIndexCtrl', [ 'Auth', '$scope', '$state', '$stateParams', 'Restangular', 'RecipeService', 'savedRecipeService', 'myRecipeService', 'madeRecipeService', function( Auth, $scope, $state, $stateParams, Restangular, RecipeService, savedRecipeService, myRecipeService, madeRecipeService ) {
+reciffy.controller( 'RecipeIndexCtrl', [ 'Auth', '$scope', '$state', '$stateParams', 'Restangular', 'RecipeService', 'savedRecipeService', 'myRecipeService', 'madeRecipeService', 'currentUser', function( Auth, $scope, $state, $stateParams, Restangular, RecipeService, savedRecipeService, myRecipeService, madeRecipeService, currentUser ) {
+
+  $scope.currentUser = currentUser;
 
   RecipeService.setRecipes();
   savedRecipeService.callAllSavedRecipes();
 
   $scope.recipes = RecipeService.getRecipes();
   $scope.savedRecipes = savedRecipeService.getSavedRecipes();
+
+  madeRecipeService.getAllMadeRecipes();
+  $scope.madeRecipes = madeRecipeService.getMadeRecipes();
+
+  myRecipeService.getAllMyRecipes($scope.currentUser.id);
+  $scope.myRecipes = myRecipeService.getMyRecipes();
 
 }]);
