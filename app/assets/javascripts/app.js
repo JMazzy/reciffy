a835ee61a1ba3a3ca07ac9fe6d83933b1c8ac28f
Joshua Masland-Sarani <j.masland@gmail.com>
2016-04-25 15:25:31 -0400

    fixes

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -91,7 +91,10 @@ reciffy.config(['$urlRouterProvider', '$stateProvider',
       resolve: {
         currentUser: ['Auth', function(Auth) {
           return Auth.currentUser();
-        }]
+        }],
+        allMadeRecipes: ['Restangular', function(Restangular){
+          return Restangular.all("made_recipes").getList();
+        }],
       },
     })
     // Create Recipe Page
