55c159cfc86e7f51cf6be3e1c4dcc8dcd978992d
Deepa Kackar <dkackar99@yahoo.com>
2016-04-25 22:32:19 -0700

    Added some horizontal scrolling

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,6 @@
-var reciffy = angular.module('reciffy', ['ui.router', 'restangular', 'Devise', 'xeditable', 'ui.bootstrap', 'angular.filter', 'angular-input-stars', 'ngFileUpload', "angucomplete-alt"])
+var reciffy = angular.module('reciffy', ['ui.router', 'restangular', 'Devise', 'xeditable',
+                                        'ui.bootstrap', 'angular.filter', 'angular-input-stars',
+                                        'ngFileUpload', "angucomplete-alt"])
 
 .config([ 'AuthProvider', function(AuthProvider) {
     // Configure Auth service with AuthProvider
