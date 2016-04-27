d0db9e83e812bced7cb79d5106e197b1beed6dba
Deepa Kackar <dkackar99@yahoo.com>
2016-04-27 16:53:31 -0700

    merged

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -1,10 +1,10 @@
 FactoryGirl.define do
   factory :recipe do
+  	user
     sequence(:name){ |n| "Recipe #{n}" }
     sequence(:description){ |n| "Description #{n}" }
     sequence(:instructions){ |n| "Instructions #{n}" }
     prep_time 1
     cook_time 1
-    user
   end
 end
