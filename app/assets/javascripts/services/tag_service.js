8966769c8ec34d1aec9dac7e1f532a460e7964c3
Joshua Masland-Sarani <j.masland@gmail.com>
2016-04-28 00:12:00 -0400

    more tag refactoring

--- app/assets/javascripts/services/tag_service.js
+++ app/assets/javascripts/services/tag_service.js
@@ -1,7 +1,14 @@
 reciffy.factory('TagService', ['Restangular', 'RecipeService', function(Restangular, RecipeService) {
 
+  // An object to hold the tags which have been retrieved from the API
   var _tags = {};
 
+  // Get the tag storage object
+  var getTags = function() {
+    return _tags;
+  }
+
+  // API call all the tags (for index page)
   var callAllTags = function() {
     Restangular
     .all('tags')
@@ -13,6 +20,7 @@ reciffy.factory('TagService', ['Restangular', 'RecipeService', function(Restangu
     })
   }
 
+  // API call one tag, if not already loaded
   var callOneTag = function(tag_id) {
     if ( !_tags[tag_id] ) {
       Restangular
@@ -20,22 +28,14 @@ reciffy.factory('TagService', ['Restangular', 'RecipeService', function(Restangu
       .get()
       .then( function(tag) {
         _tags[tag.id] = tag;
-        loadAssociations(tag);
+        _loadAssociations(tag);
       });
+    } else {
+      _loadAssociations(_tags[tag_id])
     }
   };
 
-  var loadAssociations = function(tag) {
-    for ( var t = 0; t < tag.taggings.length; t++ ) {
-      var type = tag.taggings[t].taggable_type
-      var id = tag.taggings[t].taggable_id
-
-      if (type === "Recipe") {
-        RecipeService.setOneRecipe(id);
-      }
-    }
-  }
-
+  // Find one tag by the name (among loaded tags, for search)
   var findTagByName = function(tagName) {
     var temp;
     for (var tag in _tags) {
@@ -48,8 +48,16 @@ reciffy.factory('TagService', ['Restangular', 'RecipeService', function(Restangu
     }
   };
 
-  var getTags = function() {
-    return _tags;
+  // Load associated recipes, to ensure they are loaded for display
+  var _loadAssociations = function(tag) {
+    for ( var t = 0; t < tag.taggings.length; t++ ) {
+      var type = tag.taggings[t].taggable_type
+      var id = tag.taggings[t].taggable_id
+
+      if (type === "Recipe") {
+        RecipeService.setOneRecipe(id);
+      }
+    }
   }
 
   return {
